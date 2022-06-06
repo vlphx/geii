c07e365ac355c23d290030c922fee6270ec5701e
muthuvel15 <muthuvel.savoundirapandiane@outlook.com>
2022-06-06 20:53:26 +0200

    update db.js

--- app/config/db.js
+++ app/config/db.js
@@ -1,11 +1,10 @@
-
 // https://www.bezkoder.com/sequelize-associate-many-to-many/
 //const {createConnection}  = require('mysql');
-module.exports ={
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'geii_db2',
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'geii_db',
   dialect: 'mysql',
   // First five parameters are for MySQL connection.
   // pool is optional, it will be used for Sequelize connection pool configuration:
@@ -14,7 +13,7 @@ module.exports ={
   //     min: minimum number of connection in pool
   //     idle: maximum time, in milliseconds, that a connection can be idle before being released
   //     acquire: maximum time, in milliseconds, that pool will try to get connection before throwing error
-    pool: {
+  pool: {
     max: 5,
     min: 0,
     acquire: 30000,
